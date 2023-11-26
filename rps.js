a9a9e2b9aaa008285d5609c82edb33bade0b2109
Aydin Joshi <99849099+Ajoshi1336@users.noreply.github.com>
2023-11-26 09:49:47 -0500

    Version 1.6.5

--- rps.js
+++ rps.js
@@ -1,50 +1,29 @@
 let userWins = 0;
 let cpuWins = 0;
 let gameActive = true;
-let winsToWinGame = 10; // Default value for number of wins to win the game
+let winsToWinGame = 0;
 
-document.getElementById('winCountForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent default form submission behavior
-    
-    // Get input value and update winsToWinGame
-    const inputWinCount = document.getElementById('winCount');
-    winsToWinGame = parseInt(inputWinCount.getAttribute('value'));
-    
+function startGame() {
+    winsToWinGame = parseInt(prompt("Enter the number of wins required to end the game (between 5 and 100):"));
+
     if (isNaN(winsToWinGame) || winsToWinGame < 5 || winsToWinGame > 100) {
         alert("Please enter a valid number between 5 and 100.");
-        inputWinCount.getAttribute('value') = 10; // Reset the input to the default value
+        startGame(); // Ask again for a valid input
     } else {
-        displayGameInfo(); // Update the game info based on the new win count
+        // Start the game
+        displayGameInfo(); // Display game information
+        // You can add any additional setup logic here if needed
     }
-});
-
-function checkWinConditions() {
-    if (userWins >= winsToWinGame) {
-        alert("Congratulations! You win!");
-        resetGame();
-    }
-
-    if (cpuWins >= winsToWinGame) {
-        alert("You lost! Reload the page to try again!");
-        resetGame();
-    }
-}
-
-function resetGame() {
-    userWins = 0;
-    cpuWins = 0;
-    gameActive = true;
-    // Other reset logic if needed
-
-    document.getElementById('wins-text').textContent = 'Wins: 0';
-    document.getElementById('losses-text').textContent = 'Losses: 0';
-    displayGameInfo(); // Update the game info based on the new win count
 }
 
 function displayGameInfo() {
     const gameInfo = document.getElementById('game-info');
     gameInfo.textContent = `First to ${winsToWinGame} wins!`; // Display the game info
 }
+
+// Function to start the game initially
+startGame();
+
 
 function playerChoice(playerSelection) {
     if (!gameActive) {
@@ -58,6 +37,7 @@ function playerChoice(playerSelection) {
 
     displayResult(playerSelection, computerSelection, result);
 
+    // Check win conditions after each round
     checkWinConditions();
 }
 
@@ -67,8 +47,10 @@ function determineWinner(player, computer) {
     } else if ((player === 'rock' && computer === 'scissors') ||
                (player === 'paper' && computer === 'rock') ||
                (player === 'scissors' && computer === 'paper')) {
+
         userWins += 1;
         return 'You win!';
+        
     } else {
         cpuWins += 1;
         return 'Computer wins!';
@@ -84,15 +66,20 @@ function displayResult(player, computer, result) {
 
     const cpuWinsText = document.getElementById('losses-text');
     cpuWinsText.textContent = `Losses: ${cpuWins}`;
+
+    
 }
 
+function checkWinConditions() {
+    if (userWins === winsToWinGame) {
+        alert("Congratulations! You win!");
+        gameActive = false; // Set the game as ended
+        window.location.reload(); // Reload the page
+    }
 
-// Function to display game information based on winsToWinGame
-function displayGameInfo() {
-    const gameInfo = document.getElementById('game-info');
-    gameInfo.textContent = `First to ${winsToWinGame} wins!`; // Display the game info
-
+    if (cpuWins === winsToWinGame) {
+        alert("You lost! Reload the page to try again!");
+        gameActive = false; // Set the game as ended
+        window.location.reload(); // Reload the page
+    }
 }
-
-// Initial call to display game information
-displayGameInfo();
